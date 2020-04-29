f71d6fd5f8d9abd84982f10b8672dc54c6e4eb94
akushagra <kushagra.agarwal@tekmindz.com>
2020-04-30 01:27:03 +0530

    Commit 0 for decorators ex.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor)  {
+  const original =descriptor.value;
+
+  descriptor.value = function(...args) {
+    const result = original.apply(this, args)
+      console.log("The result is ", result )
+    
+  return result;
+  };
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +18,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angualar-boilerplate';
+
+  constructor(){
+   console.log(this.aSimpleMethod(6)) 
+  }
+
+  @log
+  aSimpleMethod(a){
+    return a*a
+  }
 }
